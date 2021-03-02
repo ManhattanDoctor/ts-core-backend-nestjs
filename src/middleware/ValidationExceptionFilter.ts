64ee907e55a445d008a8575b06409afd7e552f7c
Renat Gubaev <>
2021-03-02 14:25:29 +0300

    no message

---
+++ src/middleware/ValidationExceptionFilter.ts
@@ -0,0 +1,38 @@
+import { ArgumentsHost } from '@nestjs/common';
+import { ValidationError } from 'class-validator/validation/ValidationError';
+import { ObjectUtil, TransformUtil, ValidateUtil } from '@ts-core/common/util';
+import * as _ from 'lodash';
+import { ExtendedError } from '@ts-core/common/error';
+import { IExceptionFilter } from './IExceptionFilter';
+
+export class ValidationExceptionFilter implements IExceptionFilter<ValidationException> {
+    // --------------------------------------------------------------------------
+    //
+    //  Protected Methods
+    //
+    // --------------------------------------------------------------------------
+
+    public instanceOf(exception: any): exception is ValidationException {
+        if (!ObjectUtil.hasOwnProperties(exception, ['status', 'message', 'response'])) {
+            return false;
+        }
+        let items = exception.response.message;
+        return _.isArray(items) ? items.every(item => item instanceof ValidationError) : false;
+    }
+
+    public catch(exception: ValidationException, host: ArgumentsHost): void {
+        let context = host.switchToHttp();
+        let response = context.getResponse();
+
+        let error = new ExtendedError(ValidateUtil.toString(exception.response.message), exception.response.statusCode, exception.response.message);
+        response.status(error.code).json(TransformUtil.fromClass(error));
+    }
+}
+
+export class ValidationException extends Error implements Error {
+    public response: {
+        error: string;
+        message: Array<ValidationError>;
+        statusCode: number;
+    };
+}
